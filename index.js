b3cbe782fba9694128e5424106280a88e0dc905c
cojored <66964845+cojored@users.noreply.github.com>
2020-09-17 14:12:35 -0400

    KERMIT

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const addspace = require(__dirname + '/data/addspace.js');
+function space(string) {
+  if (typeof string !== "string") throw new TypeError("Please enter a string");
+  first = string.split(" ").join(" ")
+  return first.split("").join(" ")
+};
+
+function spacecaps(string) {
+  if (typeof string !== "string") throw new TypeError("Please enter a string");
+  first = string.toUpperCase().split(" ").join(" ")
+  return first.split("").join(" ")
+};
+
+function caps(string) {
+  if (typeof string !== "string") throw new TypeError("Please enter a string");
+  return string.toUpperCase()
+};
+
+function lowercase(string) {
+  if (typeof string !== "string") throw new TypeError("Please enter a string");
+  return string.toLowerCase()
+};
+
+
+module.exports = {space, spacecaps, caps, lowercase}
